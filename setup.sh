6286735dfd83964bf7913d3e88b0237b9279cb62
alstjr7375 <alstjr7375@daum.net>
2022-05-14 02:50:27 +0900

    Fix: yarn init with workspace

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 # == Base =====================================================================
 # Init yarn3 with corepack
-yarn init -2
+# https://dev.to/arcanis/yarn-31-corepack-esm-pnpm-optional-packages--3hak
+yarn init -2 -w
 
 # Monorepo
+# https://turborepo.org/docs/getting-started#install-turbo
 yarn add -D turbo
 
 # Typescript
+# https://dev.to/mxro/the-ultimate-guide-to-typescript-monorepos-5ap7
 yarn add -D typescript@next
 yarn run tsc --init
