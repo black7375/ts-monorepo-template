d6d758725d47170bd51ec35550f806a9041cff93
alstjr7375 <alstjr7375@daum.net>
2022-05-20 17:45:27 +0900

    Bump: yarn version to latest

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 # Init yarn3 with corepack
 # https://dev.to/arcanis/yarn-31-corepack-esm-pnpm-optional-packages--3hak
 yarn init -2 -w
+yarn config set nodeLinker pnpm # pnp | pnpm | node-modules
+# yarn config set pnpEnableEsmLoader true # if use pnp mode
 
 # Monorepo
 # https://turborepo.org/docs/getting-started#install-turbo
