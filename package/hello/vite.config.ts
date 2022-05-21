fa73b1aeeee25ca6cbd4317543707397ba4ac312
alstjr7375 <alstjr7375@daum.net>
2022-05-22 01:06:16 +0900

    Add: vite plugin compose

--- package/hello/vite.config.ts
+++ package/hello/vite.config.ts
@@ -1,9 +1,10 @@
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import dts from 'vite-plugin-dts';
 
+// == Vite Config =============================================================
 // https://vitejs.dev/config/#build-lib
-export default defineConfig({
+export default ({ command, mode }: typeof process.env) => defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
@@ -11,5 +12,31 @@ export default defineConfig({
       fileName: (format) => (format === "es") ? "index.mjs" : "index.cjs"
     }
   },
-  plugins: [dts()]
+  plugins: pluginOption(command, mode)
 });
+
+// == Plugin ==================================================================
+function pluginOption(command: string, mode: string) {
+  const options = new PluginBuilder();
+  if (command === "build") {
+    options.add( dts() );
+  }
+  return options.build();
+}
+
+class PluginBuilder {
+  private readonly _plugins: PluginOption[];
+
+  constructor(plugins: PluginOption[] = []) {
+    this._plugins = plugins;
+  }
+
+  add(...plugins: PluginOption[]): PluginBuilder {
+    plugins.forEach(plugin => this._plugins.push( plugin ));
+    return this;
+  }
+
+  build(): PluginOption[] {
+    return this._plugins;
+  }
+}
