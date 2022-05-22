547016a022189a65d300b1ff33771a65dd2b71cf
alstjr7375 <alstjr7375@daum.net>
2022-05-22 23:44:51 +0900

    Add: debug / relase mode
    - debug: sourcemap, no minimize
    - relaese: no sourcemap, minimize

--- package/hello/vite.config.ts
+++ package/hello/vite.config.ts
@@ -1,24 +1,111 @@
 import { resolve } from "path";
-import { defineConfig, PluginOption } from "vite";
+import { defineConfig, ConfigEnv, UserConfig, PluginOption } from "vite";
 import dts from 'vite-plugin-dts';
 
 // == Vite Config =============================================================
 // https://vitejs.dev/config/#build-lib
-export default ({ command, mode }: typeof process.env) => defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es", "cjs"],
-      fileName: (format) => (format === "es") ? "index.mjs" : "index.cjs"
+export default ({ command, mode }: ConfigEnv) => {
+  Build.set(command, mode);
+  return defineConfig({
+    ...userOption(),
+    plugins: pluginOption()
+  });
+};
+
+class Build {
+  private static COMMAND: ConfigEnv["command"];
+  private static MODE: ConfigEnv["mode"];
+
+  public static set(command: ConfigEnv["command"], mode: ConfigEnv["mode"]) {
+    Build.COMMAND = command;
+    Build.MODE = mode;
+  }
+  public static isDev() {
+    return (Build.COMMAND === "build" && Build.MODE === "development");
+  }
+  public static isProd() {
+    return (Build.COMMAND === "build" && Build.MODE === "production");
+  }
+}
+
+// == Config ==================================================================
+function userOption() {
+  const configs = new UserConfigBuilder({
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        formats: ["es", "cjs"],
+        fileName: (format) => (format === "es") ? "index.mjs" : "index.cjs"
+      }
     }
-  },
-  plugins: pluginOption(command, mode)
-});
+  });
+  if (Build.isDev()) {
+    configs.add({
+      build: {
+        sourcemap: true,
+        minify: false
+      }
+    });
+  }
+  if (Build.isProd()) {
+    configs.add({
+      build: {
+        sourcemap: false,
+        minify: "terser"
+      }
+    });
+  }
+  return configs.build();
+}
+
+class UserConfigBuilder {
+  private readonly _configs: UserConfig;
+
+  constructor(configs: UserConfig = {}) {
+    this._configs = configs;
+  }
+
+  private isObject(obj: UserConfig[keyof UserConfig]) {
+    return !!obj && typeof obj === 'object'
+  }
+
+  private merge(target: UserConfig, source: UserConfig) {
+    if (!this.isObject(target) || !this.isObject(source)) {
+      return source;
+    }
+  
+    Object.keys(source).forEach(key => {
+      const targetValue = target[key];
+      const sourceValue = source[key];
+  
+      if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+        target[key] = targetValue.concat(sourceValue);
+      }
+      else if (this.isObject(targetValue) && this.isObject(sourceValue)) {
+        target[key] = this.merge(Object.assign({}, targetValue), sourceValue);
+      }
+      else {
+        target[key] = sourceValue;
+      }
+    });
+  
+    return target;
+  }
+
+  add(...configs: UserConfig[]) {
+    configs.forEach(config => this.merge(this._configs, config));
+    return this;
+  }
+
+  build(): UserConfig {
+    return this._configs;
+  }
+}
 
 // == Plugin ==================================================================
-function pluginOption(command: string, mode: string) {
+function pluginOption() {
   const options = new PluginBuilder();
-  if (command === "build" && mode === "production") {
+  if (Build.isProd()) {
     options.add( dts() );
   }
   return options.build();
@@ -31,7 +118,7 @@ class PluginBuilder {
     this._plugins = plugins;
   }
 
-  add(...plugins: PluginOption[]): PluginBuilder {
+  add(...plugins: PluginOption[]) {
     plugins.forEach(plugin => this._plugins.push( plugin ));
     return this;
   }
